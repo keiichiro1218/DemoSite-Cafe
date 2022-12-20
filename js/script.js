855e377c94884b84c9db73d964ed1b18ddaa572b
千葉桂一郎 <chibakeiichirou@MacBook-Air.local>
2022-12-20 21:56:08 +0900

    first commit

--- js/script.js
+++ js/script.js
@@ -1,11 +1,7 @@
 
 $(function(){
     
-    $(".p-hamburgerbtn__openbtn").click(function () {
-        $(this).toggleClass('active');
-        $(".p-headermenu, body").toggleClass('active');
-    });
-    /* ここにjQueryのコードを書く */
+    
      
     });
     
